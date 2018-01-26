c8ffb81466b445324de2ef82fb1d0fd7c8da8c7b
KeltarHelviett <keltarhelviett@gmail.com>
2018-01-26 20:56:03 +1000

    Added english localization

--- ParseConfig.py
+++ ParseConfig.py
@@ -3,12 +3,7 @@
 from os import environ
 import json
 
-lang = 'ru'
-
 CONFIG = json.load(open('config.json'))
-
-def set_lang(_lang):
-    lang = _lang
 
 def get_env():
     """Return enviroment ['dev' or 'prod']"""
@@ -35,12 +30,30 @@ def get_db_config():
                  'status': CONFIG['tables']['status']}
            }
 
-def get_reg_btns():
+def get_reg_btns(lang):
     """Return dict with buttons text for registration"""
 
+    lang = format_lang(lang)
     return CONFIG['lang'][lang]['menus']['reg_menu']
 
-def get_main_menu_btns():
+def get_main_menu_btns(lang):
     """Return dict with buttons text for main menu"""
 
+    lang = format_lang(lang)
     return CONFIG['lang'][lang]['menus']['main_menu']
+
+def get_conversations(lang, *args):
+    """ Return dict with conversations messages according to language """
+
+    lang = format_lang(lang)
+    r = CONFIG['lang'][lang]['conversations']
+
+    for e in args:
+        r = r[e]
+
+    return r
+
+def format_lang(lang):
+    """ """
+
+    return lang.split('-')[0]
--- main.py
+++ main.py
@@ -16,39 +16,42 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 LOGGER = logging.getLogger(__name__)
 
-REG_BTNS = ParseConfig.get_reg_btns()
+REG_BTNS = ParseConfig.get_reg_btns('ru-Ru')
 PHONE, ROOM = range(2)
 
-MAIN_MENU_BTNS = ParseConfig.get_main_menu_btns()
+MAIN_MENU_BTNS = ParseConfig.get_main_menu_btns('ru-Ru')
 MAIN_MENU, UPDATE_ROOM, COMPLAINT, CHANGE_STATUS = range(4)
+CONVERSATIONS = ParseConfig.get_conversations('ru-Ru')
 
 def start(bot, update):
     """/start command"""
 
+    lang = update.effective_user.language_code
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
     reg_status = DBWorker.get_reg_status(update.message.chat_id)
 
     if reg_status == RegStatus.NO_ROOM:
-        update.message.reply_text('У меня уже есть твой мобильный.' +
-                                  'Теперь тебе надо ввести номер своей комнаты.')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'registration', 'no_room'))
 
         return ROOM
     elif reg_status == RegStatus.COMPLETE:
-        update.message.reply_text('Ты уже зарегестрирован ;) Для начала введи /main')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'registration', 'already_registered'))
 
         return ConversationHandler.END
 
     phone_btn = KeyboardButton(text=REG_BTNS['share_phone_number'], request_contact=True)
     keyboard = ReplyKeyboardMarkup([[phone_btn]], one_time_keyboard=True)
 
-    update.message.reply_text('Привет! Мне нужен твой номер телефона :)',
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'registration', 'need_phone_number'),
                               reply_markup=keyboard)
 
     return PHONE
 
 def phone(bot, update):
     """Get user phone"""
+
+    lang = update.effective_user.language_code
 
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
@@ -63,8 +66,7 @@ def phone(bot, update):
 
     LOGGER.info('User was registered: ' + str(chat_id) + ' | ' + str(phone_number))
 
-    update.message.reply_text('Теперь введи номер своей комнаты. Он должен состоять из 5-ти цифр, '
-                              'как на твоем пропуске.',
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'registrations', 'room_number'),
                               reply_markup=ReplyKeyboardRemove())
 
     return ROOM
@@ -72,13 +74,14 @@ def phone(bot, update):
 def room(bot, update):
     """Register user room"""
 
+    lang = update.effective_user.language_code
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
     if not DBWorker.update_room(update.message.chat_id, update.message.text):
-        update.message.reply_text('Некоректный номер комнаты. Попробуй еще раз :)')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'registration', 'incorrect_room_number'))
         return ROOM
 
-    update.message.reply_text('Спасибо! Введи /main, чтобы увидеть на что я способен.')
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'registration', 'finished_registration'))
 
     return ConversationHandler.END
 
@@ -86,10 +89,9 @@ def main_menu(bot, update):
     """Show main menu"""
 
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-
+    lang = update.effective_user.language_code
     if DBWorker.get_reg_status(update.message.chat_id) != RegStatus.COMPLETE:
-        update.message.reply_text('Вы заполнили не все данные о себе. '
-                                  'Введи /start для регистрации.')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'uncomplete_registration'))
 
         return ConversationHandler.END
 
@@ -100,7 +102,7 @@ def main_menu(bot, update):
     keyboard = ReplyKeyboardMarkup([[change_room_btn, send_complaint_btn], [change_status_btn]],
                                    one_time_keyboard=True)
 
-    update.message.reply_text('Главное меню',
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'main_menu'),
                               reply_markup=keyboard)
 
     return MAIN_MENU
@@ -111,11 +113,10 @@ def main_menu_handler(bot, update):
     cmd = update.message.text
 
     if cmd == MAIN_MENU_BTNS['change_room']:
-        update.message.reply_text('Введите новый номер комнаты. Он должен состоять из 5-ти цифр, '
-                                  'как на твоем пропуске.')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'change_room'))
         return ROOM
     elif cmd == MAIN_MENU_BTNS['send_complaint']:
-        update.message.reply_text('Введите свою жалобу.')
+        update.message.reply_text()
         return COMPLAINT
     elif cmd == MAIN_MENU_BTNS['change_status']:
         update.message.reply_text('Введите статус своей комнаты.')
@@ -125,12 +126,12 @@ def main_menu_update_room(bot, update):
     """Update room number from main menu"""
 
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-
+    lang = update.effective_user.language_code
     if not DBWorker.update_room(update.message.chat_id, update.message.text):
-        update.message.reply_text('Некоректный номер комнаты. Попробуй еще раз :)')
+        update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'incorrect_room_number'))
         return UPDATE_ROOM
 
-    update.message.reply_text('Комната успешно изменена.')
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'changed_room_number'))
 
     return MAIN_MENU
 
@@ -138,18 +139,18 @@ def add_complaint(bot, update):
     """Add complaint"""
 
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-
+    lang = update.effective_user.language_code
     DBWorker.add_complaint(update.message.chat_id, update.message.text)
-    update.message.reply_text('Спасибо. Мы попытаемся решить вашу проблему :)')
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'added_complaint'))
 
     return MAIN_MENU
 
 def change_room_status(bot, update):
     """Change room status"""
-
+    lang = update.effective_user.language_code
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-    DBWorker.change_room_status(DBWorker.get_room(update.message.chat_id), update.message.text)
-    update.message.reply_text('Статус успешно изменен.')
+    DBWorker.change_room_status(DBWorker.get_room_number(update.message.chat_id), update.message.text)
+    update.message.reply_text(ParseConfig.get_conversations(lang, 'main', 'status_successfully_changed'))
 
     return MAIN_MENU
 
@@ -175,9 +176,9 @@ def main():
     dpt = updater.dispatcher
 
     reg_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points = [CommandHandler('start', start)],
 
-        states={
+        states = {
             PHONE: [MessageHandler(Filters.contact, phone)],
             ROOM: [MessageHandler(Filters.text, room)]
         },
